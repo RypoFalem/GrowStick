6b3be8ca0d067b9086423a8f9b1c0a1948fcfd07
RypoFalem <rypofalem@gmail.com>
2016-06-21 10:29:56 -0500

    Configure ALL the things!

--- src/io/github/rypofalem/growstick/GrowStickPlugin.java
+++ src/io/github/rypofalem/growstick/GrowStickPlugin.java
@@ -1,5 +1,7 @@
 package io.github.rypofalem.growstick;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -24,15 +26,48 @@ import com.winthier.skills.*;
 public class GrowStickPlugin extends JavaPlugin implements Listener{
 	Random rand; 
 	Skill harvest;
+	float baseMultiplier = .2f;
+	float skillMultiplier = .005f;
 	boolean skillsEnabled = false;
-	
+	ArrayList<Material> doNotUpdateList;
+
 	@Override
 	public void onEnable(){
 		Bukkit.getPluginManager().registerEvents(this, this);
 		rand = new Random();
 		skillsEnabled = Bukkit.getPluginManager().isPluginEnabled("Skills");
+		loadConfig();
 	}
-	
+
+	void loadConfig(){
+		saveDefaultConfig();
+		reloadConfig();
+		if(getConfig() == null) return;
+		doNotUpdateList = new ArrayList<Material>();
+		
+		if(getConfig().contains("blacklist")){
+			List<String> blacklist = getConfig().getStringList("blacklist");
+			if(blacklist != null){
+				for(String matName : blacklist){
+					try{
+						Material mat = Material.valueOf(matName.toUpperCase());
+						doNotUpdateList.add(mat);
+					}catch(Exception e){
+						Bukkit.getLogger().info("Invalid Material name in GrowStick/config.yml: " + matName);
+					}
+				}
+			}
+		}
+		
+		if(getConfig().isDouble("baseMultiplier")){
+			baseMultiplier = (float) getConfig().getDouble("baseMultiplier");
+		}
+		
+		if(getConfig().isDouble("skillMultiplier")){
+			skillMultiplier = (float) getConfig().getDouble("skillMultiplier");
+		}
+	}
+
 	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled=true)
 	public void onRightClick(PlayerInteractEvent event){
 		if(!event.getPlayer().hasPermission("growstick.use")) return;
@@ -44,24 +79,31 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 		Block clickedBlock = event.getClickedBlock();
 		World world = clickedBlock.getWorld();
 		world.playSound(clickedBlock.getLocation(), Sound.BLOCK_WATER_AMBIENT, .1f, 1);
-		
+
 		int range = getRange(event.getPlayer());
 		int length= 2*range + 1;
-		float updateBase = length * length* getMultiplier(event.getPlayer());
-		int updates = (int) Math.max(1, updateBase/5);
-		if(updateBase - (int)(updateBase) <= rand.nextFloat()){
+		float updateBase = length * length * getMultiplier(event.getPlayer());
+		int updates = (int) Math.max(1, updateBase * baseMultiplier);
+		if((updateBase - (int)(updateBase)) * baseMultiplier <= rand.nextFloat()){
 			//TODO: optimize so this happens 1/x times without random number generation
 			updates++; 
 		}
-		
+
 		for(int i = 0; i<updates; i++){
 			int x = rand.nextInt(length) - range + clickedBlock.getX();
 			int y = clickedBlock.getY();
 			int z = rand.nextInt(length) - range + clickedBlock.getZ();
 			for(int yOffset = 2; yOffset >= -1; yOffset--){
 				Block crop = world.getBlockAt(x, y + yOffset, z);
-				if(crop.getType() == Material.PORTAL) continue; //don't make pigmen spawn
-				if(crop.getType() == Material.REDSTONE_LAMP_ON) continue; //don't update redstone lamps that are glitched on
+				Material cropType = crop.getType();
+				boolean isBlacklisted = false;
+				for(Material mat : doNotUpdateList){
+					if(cropType.equals(mat)){
+						isBlacklisted = true;
+						break;
+					}
+				}
+				if(isBlacklisted) continue;
 				if(crop.getType() != Material.AIR){ //any other block is safe to schedule an update for
 					Location blockLoc = new Location(world, x + .5, y + yOffset + .99, z + .5);
 					BlockUpdater.update(blockLoc);
@@ -69,26 +111,26 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 				}
 			}
 		}
-		
+
 		for(int xOffset = range * -1; xOffset <= range; xOffset++){
 			for(int zOffset = range * -1; zOffset <= range; zOffset++){
 				world.playEffect(clickedBlock.getLocation().clone().add(xOffset + .5, 1, zOffset + .5), Effect.SPLASH, 1);
 			}
 		}
 	}
-	
+
 	float getMultiplier(Player player){
-		return 1 + getSkillLevel(player) * .005f;
+		return 1 + getSkillLevel(player) * skillMultiplier;
 	}
-	
+
 	int getRange(Player player){
 		return Math.min(1 + (int)(getSkillLevel(player)/100), 4); 
 	}
-	
+
 	int getSkillLevel(Player player){
 		if(!skillsEnabled) return 0;
 		Skills skills = Skills.getInstance();
-		
+
 		harvest = null;
 		if(harvest == null){
 			if(skills == null) return 0;
@@ -100,12 +142,12 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 			}
 			if(harvest == null) return 0;
 		}
-		
+
 		return skills.getScore().getSkillLevel(player.getUniqueId(), harvest);
 	}
-	
+
 	static void print(String message){
 		Bukkit.getServer().broadcastMessage(message);
 	}
-	
+
 }
