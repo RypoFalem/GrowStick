5af708d2b6ba7554d9111f9a2ae331d4ef2a9703
RypoFalem <rypofalem@gmail.com>
2018-07-21 13:40:20 -0500

    update to 1.13

--- src/main/java/io/github/rypofalem/growstick/BlockUpdater.java
+++ src/main/java/io/github/rypofalem/growstick/BlockUpdater.java
@@ -1,18 +1,31 @@
 package io.github.rypofalem.growstick;
 
-import net.minecraft.server.v1_12_R1.BlockPosition;
+import net.minecraft.server.v1_13_R1.Block;
+import net.minecraft.server.v1_13_R1.BlockPosition;
+import net.minecraft.server.v1_13_R1.IBlockData;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers;
+import org.bukkit.craftbukkit.v1_13_R1.block.data.CraftBlockData;
+import org.bukkit.craftbukkit.v1_13_R1.util.CraftMagicNumbers;
+
+import java.util.Random;
 
 
 public abstract class BlockUpdater {
-	
+	private static Random random = new Random();
+
 	//schedules a "random tick" block update
 	//see https://minecraft.gamepedia.com/Tick#Block_tick
 	static void update(Location loc){
-		net.minecraft.server.v1_12_R1.World mcWorld = ((org.bukkit.craftbukkit.v1_12_R1.CraftWorld) loc.getWorld()).getHandle();
+		net.minecraft.server.v1_13_R1.World mcWorld = ((org.bukkit.craftbukkit.v1_13_R1.CraftWorld) loc.getWorld()).getHandle();
 		BlockPosition blockPos = new BlockPosition(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-		mcWorld.a(blockPos, CraftMagicNumbers.getBlock(loc.getBlock()), 1);
-	}
 
+		org.bukkit.block.Block bukkitBlock = loc.getBlock();
+		if(!(bukkitBlock.getBlockData() instanceof CraftBlockData)) return;
+
+		IBlockData blockData = ((CraftBlockData)bukkitBlock.getBlockData()).getState();
+		Block block = CraftMagicNumbers.getBlock(bukkitBlock.getType());
+
+		//mcWorld.a(blockPos, CraftMagicNumbers.getBlock(loc.getBlock().getType()), 1);
+		block.a(blockData, mcWorld, blockPos, random);
+	}
 }
--- src/main/java/io/github/rypofalem/growstick/GrowStickPlugin.java
+++ src/main/java/io/github/rypofalem/growstick/GrowStickPlugin.java
@@ -12,7 +12,6 @@ import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -22,21 +21,16 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.winthier.skills.*;
 
 public class GrowStickPlugin extends JavaPlugin implements Listener{
-	Random rand; 
-	Skill harvest;
+	Random rand;
 	float baseMultiplier = .2f;
-	float skillMultiplier = .005f;
-	boolean skillsEnabled = false;
 	ArrayList<Material> doNotUpdateList;
 
 	@Override
 	public void onEnable(){
 		Bukkit.getPluginManager().registerEvents(this, this);
 		rand = new Random();
-		skillsEnabled = Bukkit.getPluginManager().isPluginEnabled("Skills");
 		loadConfig();
 	}
 
@@ -44,7 +38,7 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 		saveDefaultConfig();
 		reloadConfig();
 		if(getConfig() == null) return;
-		doNotUpdateList = new ArrayList<Material>();
+		doNotUpdateList = new ArrayList<>();
 
 		if(getConfig().contains("blacklist")){
 			List<String> blacklist = getConfig().getStringList("blacklist");
@@ -63,10 +57,6 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 		if(getConfig().isDouble("baseMultiplier")){
 			baseMultiplier = (float) getConfig().getDouble("baseMultiplier");
 		}
-
-		if(getConfig().isDouble("skillMultiplier")){
-			skillMultiplier = (float) getConfig().getDouble("skillMultiplier");
-		}
 	}
 
 	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled=true)
@@ -81,14 +71,15 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 		World world = clickedBlock.getWorld();
 		world.playSound(clickedBlock.getLocation(), Sound.BLOCK_WATER_AMBIENT, .1f, 1);
 
-		int range = getRange(event.getPlayer());
-		int length= 2*range + 1;
-		float updateBase = length * length * getMultiplier(event.getPlayer()) * baseMultiplier;
+		int range = 1;
+		int length= 2 * range + 1;
+		float updateBase = length * length * baseMultiplier;
 
-		int updates = (int) Math.max(1, updateBase);
-		if((updateBase - (int)(updateBase)) <= rand.nextFloat()){
-			//TODO: optimize so this happens 1/x times without random number generation
-			updates++; 
+		// since there might be a fractional chance to update but updates always happen in integer amounts,
+		// separate the fractional chance and use that to decide if an extra update should be added
+		int updates = (int) updateBase;
+		if((updateBase - (int)(updateBase)) >= rand.nextFloat()){
+			updates++;
 		}
 
 		for(int i = 0; i<updates; i++){
@@ -110,37 +101,10 @@ public class GrowStickPlugin extends JavaPlugin implements Listener{
 			for(int zOffset = range * -1; zOffset <= range; zOffset++){
 				Location loc = clickedBlock.getLocation().clone().add(xOffset + .5, 1, zOffset + .5);
 				world.spawnParticle(Particle.WATER_SPLASH, loc, 1);
-				//world.playEffect(clickedBlock.getLocation().clone().add(xOffset + .5, 1, zOffset + .5), Effect.SPLASH, 1);
 			}
 		}
 	}
 
-	float getMultiplier(Player player){
-		return 1 + getSkillLevel(player) * skillMultiplier;
-	}
-
-	int getRange(Player player){
-		return Math.min(1 + (int)(getSkillLevel(player)/100), 4); 
-	}
-
-	int getSkillLevel(Player player){
-		if(!skillsEnabled) return 0;
-		Skills skills = Skills.getInstance();
-
-		harvest = null;
-		if(harvest == null){
-			if(skills == null) return 0;
-			for(Skill s : skills.getSkills()){
-				if(s.getKey().equals("harvest")){
-					harvest = s;
-					break;
-				}
-			}
-			if(harvest == null) return 0;
-		}
-
-		return skills.getScore().getSkillLevel(player.getUniqueId(), harvest);
-	}
 
 	static void print(String message){
 		Bukkit.getServer().broadcastMessage(message);
